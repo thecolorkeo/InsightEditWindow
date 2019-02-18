7a1953a5bdbc6a77914c71fc64e5ed35c6bd8ff5
Ubuntu <ubuntu@ip-10-0-0-9.ec2.internal>
2019-02-18 20:46:57 +0000

    fixed bug in spark-up-history.sh

--- spark-up-history.sh
+++ spark-up-history.sh
@@ -4,6 +4,5 @@ spark-submit --master spark://ec2-3-93-99-240.compute-1.amazonaws.com:7077 \
 	     --driver-memory 28g \
 	     --executor-memory 6500m \
 	     --executor-cores 1 \
-	     --packages com.databricks:spark-xml_2.11:0.4.1, \
-			org.postgresql:postgresql:42.2.5 \
+	     --packages com.databricks:spark-xml_2.11:0.4.1,org.postgresql:postgresql:42.2.5 \
 	     databricks-history.py $1
